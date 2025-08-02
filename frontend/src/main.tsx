7737484c6a1c06d4d7798dceaa6ac13a26789f43
Mykolai Skydan <117273969+ewqwdd@users.noreply.github.com>
2025-08-02 08:05:52 +0300

    vite config fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { store } from './app/store/store';
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.VITE_ROUTER_BASE_URL}>
     <Provider store={store}>
       <App />
       <Toaster position="top-right" />
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths(), react(), svgr()],
-  clearScreen: false,
-  define: {
-    'process.env': {},
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [tsconfigPaths(), react(), svgr()],
+    clearScreen: false,
+    base: env.VITE_BASE_URL,
+    define: {
+      'process.env': env,
+    },
+  };
 });
